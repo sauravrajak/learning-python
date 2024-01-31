426a0f140f3f2a8622d2af7c5947d12025aae77a
saurav kumar rajak <sauravrajak@outlook.com>
2024-01-31 13:08:29 +0530

    math module

---
+++ chapter-1/math.py
@@ -0,0 +1,13 @@
+# math modules in python 
+
+import math
+
+pi = 3.14
+
+print(round(pi))
+print(math.ceil(pi)) #--> 4
+print(math.floor(pi)) # --> 3
+print(abs(pi))
+print(pow(pi,2))
+print(math.sqrt(pi))
+print(max(pi,9))

808b0100bc4e8a8d1d35596a4be6d1dfd0e03d6f
saurav kumar rajak <sauravrajak@outlook.com>
2024-01-31 12:41:43 +0530

    typecasting or type conversion

---
+++ chapter-1/typecasting.py
@@ -0,0 +1,18 @@
+
+"""
+Type Conversion
+You can convert from one type to another with the
+int() , float() and complex()
+methods:
+
+"""
+
+x = 1 # int
+y = 3.14 #float
+z = "50" #string
+
+y = int(y) #int 
+z = int(z) #int 
+
+p = z+y
+print(p) # ->53

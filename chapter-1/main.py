e45d97daeb2f8f6e59d004b1ba06a8c6ee1a1e9c
saurav kumar rajak <sauravrajak@outlook.com>
2024-01-31 11:11:16 +0530

    first commit

---
+++ chapter-1/main.py
@@ -0,0 +1,95 @@
+"""
+Python is a popular general-purpose programming language. It is used in machine learning, web development, desktop applications, and many other
+fields. Fortunately for beginners, Python has a simple, easy-to-use syntax. This makes Python a great language to learn for beginners.
+
+
+Python Keywords and Identifiers
+
+Python Keywords
+
+Keywords are predefined, reserved words used in Python programming that have special
+meanings to the compiler.
+We cannot use a keyword as a variable name, function name, or any other identifier. They
+are used to define the syntax and structure of the Python language.
+All the keywords except True ,
+False and None are in lowercase and they must be
+written as they are. The list of all the keywords is given below.
+
+Python Keywords List
+
+    False
+    None
+    True
+    and
+    as
+    assert
+    async
+    await
+    break
+    class
+    continue
+    def
+    del
+    elif
+    except
+    finally
+    for
+    from
+    global
+    if
+    import
+    in
+    is
+    lambda
+    nonlocal
+    not
+    pass
+    raise
+    return
+    try
+    while
+    with
+    yield
+
+    
+
+"""
+# Python Identifiers
+# Identifiers are the name given to variables, classes, methods, etc.
+# For example,
+ 
+language = "Python"
+
+# Here, languagel is a variable (an identifier) which holds the value 'Python' .
+# We cannot use keywords as variable names as they are reserved names that are 
+# built-in
+# to Python. For example,
+
+# continue = 'Python' --> wrong syntax 
+
+
+"""
+Rules for Naming an Identifier
+    • Identifiers cannot be a keyword.
+    • Identifiers are case-sensitive.
+    • It can have a sequence of letters and digits. However, it must begin with 
+      a letter or The first letter of an identifier cannot be a digit.
+    • Itis a convention to start an identifier with a letter rather
+    • Whitespaces are not allowed.
+    • We cannot use special symbols like !, @, #, S, and so on.
+
+
+
+Things to Remember
+    Python is a case-sensitive language. This means, Variable and I variable are not the
+    same.
+    Always give the identifiers a name that makes sense. While
+        c = 10 is a valid name,
+    writing count = 10 would make more sense, and it would be easier to figure 
+    out what it
+    represents when you look at your code after a long gap.
+    Multiple words can be separated using an underscore, like .
+
+"""
+
+
